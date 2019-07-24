fb09e927ee62d995c05dc54136bd617123d73be5
Luiz Henrique Monteiro de Carvalho <38506172+LuizHMC@users.noreply.github.com>
2019-07-24 17:47:39 -0300

    Add files via upload
    Funcionalidades terminadas e funcionais

---
+++ Challenge de ferias4/ChallengeFeriasNaturezaSelvagem/ChallengeFeriasNaturezaSelvagemTests/ChallengeFeriasNaturezaSelvagemTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChallengeFeriasNaturezaSelvagemTests.swift
+//  ChallengeFeriasNaturezaSelvagemTests
+//
+//  Created by Luiz Henrique Monteiro de Carvalho on 12/07/19.
+//  Copyright © 2019 Luiz Henrique Monteiro de Carvalho. All rights reserved.
+//
+
+import XCTest
+@testable import ChallengeFeriasNaturezaSelvagem
+
+class ChallengeFeriasNaturezaSelvagemTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
